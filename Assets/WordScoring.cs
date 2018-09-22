3160e30995bdcd803460bd5e5307610588173828
Tran Tien Anh Ta (Rico) <Rico0Sandard@gmail.com>
2018-09-22 18:46:10 -0400

    TypingGame
    Everything

---
+++ Assets/WordScoring.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WordScoring : MonoBehaviour {
+
+    public WordManager wordManager;
+
+    void OnGUI()
+    {
+        GUI.Label(new Rect(0, 0, 100, 150), "Total: " + wordManager.totalWord.ToString());
+        GUI.Label(new Rect(0, 20, 100, 150), "Scored: " + wordManager.wordTyped.ToString());
+    }
+}
