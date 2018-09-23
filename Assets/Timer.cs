6f0c730dd4b43f426d1521a2ea2ff6a97db6d936
Tran Tien Anh Ta (Rico) <Rico0Sandard@gmail.com>
2018-09-23 09:12:18 -0400

    Add a timer

---
+++ Assets/Timer.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Timer : MonoBehaviour
+{
+
+    public Text timerText;
+    private float startTime;
+    public bool isRunning;
+
+    void Start()
+    {
+        startTime = Time.time;
+        isRunning = true;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (isRunning)
+        {
+            float t = Time.time - startTime;
+
+            string minutes = ((int)t / 60).ToString();
+            string seconds = (t % 60).ToString("00");
+
+            timerText.text = minutes + ":" + seconds;
+        }
+    }
+}
